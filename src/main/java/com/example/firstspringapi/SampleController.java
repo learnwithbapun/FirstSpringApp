2f60dc7f833d5832dce11c5c73d3ceca882ee2db
Bapun <learnwithbapun@gmail.com>
2024-04-27 03:37:32 +0530

    Added First Commit

---
+++ src/main/java/com/example/firstspringapi/SampleController.java
@@ -0,0 +1,27 @@
+package com.example.firstspringapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first")
+public class SampleController {
+
+    @GetMapping("/hi")
+    public String sayHello(){
+        return "Hello Bapun";
+    }
+
+    @GetMapping("/bye")
+    public String sayBye(){
+        return "Good Bye";
+    }
+
+    @GetMapping("/hello/{name}")
+    public  String SayHelloToUser(@PathVariable("name") String name){
+        return "Hello "+name;
+    }
+
+}
